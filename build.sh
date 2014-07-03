f3ecd908da27d6ee9826412ed2893c7e9e91e61e
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2014-07-03 17:52:06 +0530

    Crossbuild libusb for android using NDK
    Added build.sh script for cross compilation of libusb for android. This script
    uses standalone toolchain of Android NDK to generate libusb for android. All the
    generated libraries go in the location : crossbuild/ndk-$ARCH/sysroot/usr/lib
    where ARCH is either x86 or arm depending upon the architecture one wants to
    build on. The default is x86 as the tests are going to be conducted on emulator.
    The built library of libusb would be used later by native code on android.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

---
+++ build.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -e
+
+# Set the path to android ndk and sdk
+export ANDROID_NDK=/android_ndk
+export ANDROID_SDK=/android_sdk
+
+# Get architecture from user
+export ARCH=${1-x86}
+
+if [ "$ARCH" = "arm" ] ;
+then
+	BUILDCHAIN=arm-linux-androideabi
+	export APP_ABI=armv7
+elif [ "$ARCH" = "x86" ] ;
+then
+	BUILDCHAIN=i686-linux-android
+	export APP_ABI=x86
+fi
+
+pushd crossbuild
+
+# Initialise cross toolchain
+if [ ! -e ndk-$ARCH ] ; then
+	$ANDROID_NDK_ROOT/build/tools/make-standalone-toolchain.sh --system=linux-x86_64 --arch=$ARCH --install-dir=ndk-$ARCH --platform=android-19
+fi
+
+# Declare necessary variables for cross compilation
+export BUILDROOT=$PWD
+export PATH=${BUILDROOT}/ndk-$ARCH/bin:$PATH
+export PREFIX=${BUILDROOT}/ndk-$ARCH/sysroot/usr
+export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
+export CC=${BUILDCHAIN}-gcc
+export CXX=${BUILDCHAIN}-g++
+
+if [ ! -e libusb-1.0.9.tar.bz2 ] ; then
+	wget http://sourceforge.net/projects/libusb/files/libusb-1.0/libusb-1.0.9/libusb-1.0.9.tar.bz2
+fi
+
+if [ ! -e libusb-1.0.9 ] ; then
+	tar -jxf libusb-1.0.9.tar.bz2
+fi
+
+if ! grep -q __ANDROID__ libusb-1.0.9/libusb/io.c ; then
+	# patch libusb to build with android-ndk
+	patch -p1 < libusb-1.0.9-android.patch  libusb-1.0.9/libusb/io.c
+fi
+
+if [ ! -e $PKG_CONFIG_PATH/libusb-1.0.pc ] ; then
+	mkdir -p libusb-build-$ARCH
+	pushd libusb-build-$ARCH
+	../libusb-1.0.9/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --enable-static --disable-shared
+	make
+	make install
+	popd
+fi
+
+popd # From crossbuild
+
